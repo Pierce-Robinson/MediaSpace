82033f97a0c46ff2e44de9c7a766cc8890639db8
Liam2001-bit <liam.mark.edwards@gmail.com>
2023-10-28 14:46:29 +0200

    started with designs, check why its not with white background. added most icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.varsitycollege.mediaspace"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.varsitycollege.mediaspace"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
