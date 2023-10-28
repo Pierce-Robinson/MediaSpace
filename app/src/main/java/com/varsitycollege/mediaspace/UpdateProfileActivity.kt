09d386c14ee875163b7dbdbc439e2f6b2b28b566
Daniel Pienaar <dpienaar001@gmail.com>
2023-10-28 17:46:42 +0200

    added and linked ui activities, fragments and layouts

---
+++ app/src/main/java/com/varsitycollege/mediaspace/UpdateProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.varsitycollege.mediaspace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_profile)
+    }
+}
