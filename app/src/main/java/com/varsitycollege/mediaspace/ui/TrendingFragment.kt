542051543697028e07081dde0b990a4fe0092258
Daniel Pienaar <dpienaar001@gmail.com>
2023-10-28 17:31:42 +0200

    fixed ui issues

---
+++ app/src/main/java/com/varsitycollege/mediaspace/ui/TrendingFragment.kt
@@ -0,0 +1,20 @@
+package com.varsitycollege.mediaspace.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.varsitycollege.mediaspace.R
+
+class TrendingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trending, container, false)
+    }
+
+}
