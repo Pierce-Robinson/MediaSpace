190ee6a29634a54d0c07e6466e0519f645c1e2eb
Daniel Pienaar <dpienaar001@gmail.com>
2023-11-20 15:39:49 +0200

    Updated data classes

--- app/src/main/java/com/varsitycollege/mediaspace/data/Product.kt
+++ app/src/main/java/com/varsitycollege/mediaspace/data/Product.kt
@@ -1,14 +1,15 @@
 package com.varsitycollege.mediaspace.data
 
 data class Product(
+    val id: String ?= null,
     val sku: String ?= null,
     val stock: Int ?= null,
     val available: Boolean ?= null,
     val name: String ?= null,
     val description: String ?= null,
     val price: Double ?= null,
-    val colour: String ?= null,
-    val size: String ?= null,
+    val colourList: ArrayList<Colour> ?= null,
+    val sizeList: ArrayList<Size> ?= null,
     val categoriesList: ArrayList<String> ?= null,
     val imagesList: ArrayList<String> ?= null
 )
